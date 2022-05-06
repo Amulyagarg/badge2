d293b0297cf89d27fbaa06418a4615be4262a222
Amulyagarg <amulyagarg007@gmail.com>
2022-05-06 11:42:57 +0530

    Update streamlit_app.py
    -Reading CSV , importing Pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import Pandas
 
 streamlit.title("My New Healthy Dinner")
 streamlit.header('Breakfast Favorites')
@@ -6,8 +7,6 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
-streamlit.header('Breakfast Favorites Mine')
-streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avocado Toast Meal')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
